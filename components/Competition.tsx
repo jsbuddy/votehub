f18d01ee86010696dec60687811b6da371c953c2
jsbuddy <judehawkson@yahoo.com>
2019-05-08 01:38:20 +0100

    Attempt to fix UI bug

--- components/Competition.tsx
+++ components/Competition.tsx
@@ -49,7 +49,7 @@ const Competition = ({ competition, votingId, canVote, setVotingId, vote, curren
                             canVote && votingId !== competition._id && !userVote && <Button className="blue" text="Vote" onClick={() => setVotingId(competition._id)}></Button>
                         }
                         {
-                            (votingId === competition._id) && <>
+                            (canVote && votingId === competition._id) && <>
                                 <span><IconButton icon={<FaTimes />} disabled={voting} className="red rounded" onClick={() => setVotingId(null)}></IconButton></span>
                                 <span><IconButton icon={<FaCheck />} disabled={!selected || voting} className="green rounded" onClick={() => vote(competition._id, selected)}></IconButton></span>
                             </>
